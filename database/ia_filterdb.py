92a619fcb7cc869ba3854308d79f2fc01cf29cb9
KC Khais <khaiskc@gmail.com>
2022-04-27 18:32:34 +0530

    v1

--- database/ia_filterdb.py
+++ database/ia_filterdb.py
@@ -2,7 +2,6 @@ import logging
 from struct import pack
 import re
 import base64
-from turtle import width
 from pyrogram.file_id import FileId
 from pymongo.errors import DuplicateKeyError
 from umongo import Instance, Document, fields
